81c3303e7be50fddf3c20a467378f968e0c183f6
anik-44 <anikk3460@gmail.com>
2025-02-07 01:37:50 +0530

    fix typo

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const apiUrl = "http://api.openweathermap.org/data/2.5"
+export const apiUrl = "https://api.openweathermap.org/data/2.5"
 export const apikey = import.meta.env.VITE_OPEN_WEATHER_API_KEY
